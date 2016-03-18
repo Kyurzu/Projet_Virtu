6941bb656d1f583274bec84c3e3f0f9614768471
root <root@vanguard.montpellier.epsi.fr>
2016-03-18 11:14:24 +0100

    Modif Cree VM

--- vm-config/fonction/CreeVM.sh
+++ vm-config/fonction/CreeVM.sh
@@ -5,10 +5,20 @@ CreationVM()
 	nombre_cpu=$2
 	nombre_memoire=$3
 	taille_disque=$4
+
+	lien=`sed -n 1p system/lien.txt`
 	
-	qemu-img create -f qcow2 -o preallocation=metadata  /var/lib/libvirt/images/${nom_vm}.qcow2 ${taille_disque}M
+	qemu-img create -f qcow2 -o preallocation=metadata  ${lien}${nom_vm}.qcow2 ${taille_disque}M
 	echo -->disque dur virtuelle créer
 	virt-clone --force --original debian8-tpl --name $nom_vm --file /var/img/img-original/$nom_vm'.debian'
-	virsh destroy $nom_vm
+	echo "vm allumer"
+	virsh start $nom_vm
+	echo "debut sleep"
+	sleep 5
+	echo "setmen"
+	virsh setmem $nom_vm $nombre_memoire
+	echo "cpu"
 	virsh setvcpus --count $nombre_cpu $nom_vm
+	echo "supression vm"
+	virsh destroy $nom_vm
 }
