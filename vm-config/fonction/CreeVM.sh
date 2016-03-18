f4cc3f4d78db717d8b080748ebe3b6874fd13c21
root <root@vanguard.montpellier.epsi.fr>
2016-03-18 15:39:18 +0100

    Fixe creation vm

--- vm-config/fonction/CreeVM.sh
+++ vm-config/fonction/CreeVM.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 CreationVM()
 {
-	nom_vm=$1
-	nombre_cpu=$2
-	nombre_memoire=$3
-	taille_disque=$4
+        nom_vm=$1
+        nombre_cpu=$2
+        nombre_memoire=$3
+        taille_disque=$4
+        
+        lien=`sed -n 1p system/lien.txt`
 
-	lien=`sed -n 1p system/lien.txt`
-	
-	qemu-img create -f qcow2 -o preallocation=metadata  ${lien}${nom_vm}.qcow2 ${taille_disque}M
-	echo -->disque dur virtuelle créer
-	virt-clone --force --original debian8-tpl --name $nom_vm --file /var/img/img-original/$nom_vm'.debian'
-	echo "vm allumer"
-	virsh start $nom_vm
-	echo "debut sleep"
-	sleep 5
-	echo "setmen"
-	virsh setmem $nom_vm $nombre_memoire
-	echo "cpu"
-	virsh setvcpus --count $nombre_cpu $nom_vm
-	echo "supression vm"
-	virsh destroy $nom_vm
+	qemu-img create -f qcow2 -o preallocation=metadata ${lien}${nom_vm}.qcow2 ${taille_disque}M
+        echo -->disque dur virtuelle créer
+        virt-clone --force --original debian8-tpl --name $nom_vm --file ${lien}${nom_vm}
+
+        virsh start $nom_vm
+        virsh setvcpus --count $nombre_cpu $nom_vm
+        virsh destroy $nom_vm
+        virsh setmem $nom_vm ${nombre_memoire}M --config
+
 }
