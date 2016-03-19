71a1d65b02afb2da8398929e227ddd9bd01ed324
Username34 <yemantv@gmail.com>
2016-03-19 16:08:07 +0100

    Modif optionVM

--- vm-config/fonction/OptionVM.sh
+++ vm-config/fonction/OptionVM.sh
@@ -1,38 +1,26 @@
 #!/bin/sh
 
-Ajout_cpu_VM()
+modif_cpu_VM()
 {
+ nom_vm=$1
+ nombre_cpu=$2
+ virsh start $nom_vm
 
-echo Veuillez saisir le nom de la VM
-read choix_vm
-
-virsh shutdown $choix_vm
-
-echo Notez le nombre de CPU à ajouter
-virsh setvcpus --count $nombre_cpu $nom_vm
-
-echo Changement effectué
-virsh domingo $choix_vm | grep cpu
-
+ virsh setvcpus --count $nombre_cpu $nom_vm
+ virsh destroy $nom_vm
 	
 }
-Ajout_memoire_VM()
+modif_ram_VM()
 {
 
-echo Veuillez saisir le nom de la VM
-read choix_vm
+ nom_vm=$1
+ nombre_ram=$2
 
-virsh shutdown $choix_vm
-
-echo Notez la taille de la nouvelle RAM en KiB
-read choix_ram
-virsh setmem $choix_vm $choix_ram
-
-echo Changement effectué
-virsh dominfo $choix_vm | grep memory
+virsh destroy $nom_vm
+virsh setmem $nom_vm $nombre_ram
 	
 }
-Ajout_disque_VM()
+modif_disque_VM()
 {
 
 echo Veuillez saisir le nom de la VM
--- vm-config/page/page_option_vm.sh
+++ vm-config/page/page_option_vm.sh
@@ -19,4 +19,25 @@ echo "           Option"
 echo "1. Modifier CPU" 
 echo "2. Modifier RAM" 
 echo "3. Modifier disque"
+read choix_option
 zone_saisie
+
+case $choix_option in
+1) echo Nombre de CPU
+	zone_saisie
+   read nombre_cpu
+ 	modif_cpu_VM $nom_vm $nombre_cpu
+ 	echo Changement effectue
+;;
+2) echo Notez la taille de la nouvelle RAM en KiB
+	zone_saisie
+   read nombre_ram
+ 	modif_ram_VM $nom_vm $nombre_ram
+ 	echo Changement effectue
+;;
+;;
+3) modif_disque_VM $nom_vm
+;;
+*) echo "réponse non comprise"
+;;
+esac
